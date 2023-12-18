f5ffbca095c5f01b821573deb660ba67e2036002
yeshwanth <pingiliyeshwanthreddy728@gmail.com>
2023-12-18 21:57:32 +0530

    level 5 commit-1

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,134 +1,20 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-const todoList = require("../todo");
-const {
-  all,
-  add,
-  markAsComplete,
-  overdue,
-  dueToday,
-  dueLater,
-  //toDisplayableList,
-} = todoList();
+const db = require("../models");
 
-describe("todo-cli test suite", () => {
-  const formattedDate = (d) => {
-    return d.toISOString().split("T")[0];
-  };
-
-  var dateToday = new Date();
-  const today = formattedDate(dateToday);
-  const yesterday = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() - 1)),
-  );
-  const tomorrow = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() + 1)),
-  );
-  beforeAll(() => {
-    add(
-      {
-        title: "Submit assignment",
-        dueDate: yesterday,
-        completed: false,
-      },
-      {
-        title: "play cricket",
-        dueDate: yesterday,
-        completed: false,
-      },
-      {
-        title: "win tournament",
-        dueDate: today,
-        completed: false,
-      },
-      {
-        title: "play football",
-        dueDate: tomorrow,
-        completed: false,
-      },
-    );
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
   });
-  test("creating a new todo", () => {
-    let len = all.length;
-    let oldAll = [...all];
 
-    add({
-      title: "prepare uml",
-      dueDate: yesterday,
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.create({
+      title: "Test todo",
       completed: false,
+      dueDate: new Date(),
     });
-
-    expect(all.length).toBe(len + 1);
-    expect(all[len].title).toBe("prepare uml");
-    expect(all[len].dueDate).toBe(yesterday);
-    expect(all[len].completed).toBe(false);
-
-    oldAll.push({
-      title: "prepare uml",
-      dueDate: yesterday,
-      completed: false,
-    });
-
-    expect(oldAll).toEqual(all);
-  });
-
-  test("marking a todo as complete", () => {
-    const incompleteIndex = all.findIndex((task) => !task.completed);
-    if (incompleteIndex !== -1) {
-      markAsComplete(incompleteIndex);
-      expect(all[incompleteIndex].completed).toBe(true);
-    } else {
-      add({
-        title: "this task added when no other incomplete tasks are found",
-        dueDate: tomorrow,
-        completed: false,
-      });
-      markAsComplete(all.length - 1);
-      expect(all[all.length - 1].completed).toBe(true);
-      expect(false).toBe(true);
-    }
-  });
-
-  test("overdue retrieval", () => {
-    overdueTasks = overdue();
-    if (overdueTasks.length > 0) {
-      expect(overdueTasks[0].dueDate < today).toBe(true);
-    } else {
-      expect(false).toBe(true);
-    }
-  });
-  test("dueToday retrieval", () => {
-    dueTodayTasks = dueToday();
-    if (dueTodayTasks.length > 0) {
-      expect(dueTodayTasks[0].dueDate === today).toBe(true);
-    } else {
-      add({
-        title: "this task added when no other dueToday tasks are found",
-        dueDate: today,
-        completed: false,
-      });
-      dueTodayTasks = dueToday();
-      expect(dueTodayTasks[0].dueDate === today).toBe(true);
-      expect(dueTodayTasks[0].title).toBe(
-        "this task added when no other dueToday tasks are found",
-      );
-    }
-  });
-
-  test("dueLater retrieval", () => {
-    dueLaterTasks = dueLater();
-    if (dueLaterTasks.length > 0) {
-      expect(dueLaterTasks[0].dueDate > today).toBe(true);
-    } else {
-      add({
-        title: "this task added when no other dueLater tasks are found",
-        dueDate: tomorrow,
-        completed: false,
-      });
-      dueLaterTasks = dueLater();
-      expect(dueLaterTasks[0].dueDate > today).toBe(true);
-      expect(dueLaterTasks[0].title).toBe(
-        "this task added when no other dueLater tasks are found",
-      );
-    }
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
---
+++ todo-cli/models/todo.js
@@ -0,0 +1,126 @@
+// models/todo.js
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static async addTask(params) {
+      return await Todo.create(params);
+    }
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      // FILL IN HERE
+      const overdueTasks = await Todo.overdue();
+      const overdueTasksString = overdueTasks
+        .map((task) => task.displayableString())
+        .join("\n");
+      console.log(overdueTasksString);
+      console.log("\n");
+
+      console.log("Due Today");
+      // FILL IN HERE
+      const dueTodayTasks = await Todo.dueToday();
+      const dueTodayTasksString = dueTodayTasks
+        .map((task) => task.displayableString())
+        .join("\n");
+      console.log(dueTodayTasksString);
+
+      console.log("\n");
+
+      console.log("Due Later");
+      // FILL IN HERE
+      const dueLaterTasks = await Todo.dueLater();
+      const dueLaterTasksString = dueLaterTasks
+        .map((task) => task.displayableString())
+        .join("\n");
+      console.log(dueLaterTasksString);
+    }
+
+    static async overdue() {
+      // FILL IN HERE TO RETURN OVERDUE ITEMS
+      const today = new Date().toISOString().split("T")[0];
+      try {
+        const overdueItems = await Todo.findAll({
+          where: {
+            dueDate: {
+              $lt: today,
+            },
+            completed: false,
+          },
+        });
+        return overdueItems;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
+    }
+
+    static async dueToday() {
+      // FILL IN HERE TO RETURN ITEMS DUE tODAY
+      const today = new Date().toISOString().split("T")[0];
+      try {
+        const dueTodayItems = await Todo.findAll({
+          where: { dueDate: today },
+        });
+        return dueTodayItems;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    static async dueLater() {
+      // FILL IN HERE TO RETURN ITEMS DUE LATER
+      const today = new Date().toISOString().split("T")[0];
+      try {
+        const dueLaterItems = await Todo.findAll({
+          where: {
+            dueDate: {
+              $gt: today,
+            },
+            completed: false,
+          },
+        });
+        return dueLaterItems;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    static async markAsComplete(id) {
+      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
+      try {
+        await Todo.update(
+          { completed: true },
+          {
+            where: { id: id },
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    },
+  );
+  return Todo;
+};
